94f02e786f5d38caea27dbedfedda0acb16a3675
subhath abeysekara <subhath@liveroom.dev>
2023-08-08 00:42:55 +0530

    change template

--- sendMail.py
+++ sendMail.py
@@ -12,14 +12,15 @@ def send_verification_email(receiver_email, verification_code , userCode , type 
     <head>
     </head>
     <body style="font-family: Verdana, Geneva, Tahoma, sans-serif;">
-        <div style="display: flex; justify-content: center; align-items: center; margin-left: 25%;">
-            <div 
-                style="height: 510px; 
-                    width: 520px; 
-                    background-color: #fdfbf0; 
-                    box-shadow: 3px 2px 3px rgba(129, 129, 129, 0.5);">
+<div 
+            style="height: 510px; 
+                width: 520px; 
+                background-color: #fdfbf0; 
+                margin-left: auto;
+                margin-right: auto;
+                box-shadow: 3px 2px 3px rgba(129, 129, 129, 0.5);">
              
-                <img src="'''+f'{image_url}'+'''" alt="Lunch Bucket Logo" style="height: 100px; width: 100px; margin-top: 10px; margin-left: 210px; ">
+                <img src="'''+f'{image_url}'+'''" alt="Lunch Bucket Logo" style="height: 100px; width: 100px; margin-top: 10px; margin-left: 40%; ">
             
                 <h3 style="color: #7E1F24 ; text-align: center;">Verify your email address</h3>
                 <p style="text-align: center; font-size: 14px; font-weight: 500;">Thank you for signing up with '''+f'{name}'+'''.</p>
@@ -36,12 +37,11 @@ def send_verification_email(receiver_email, verification_code , userCode , type 
                         font-size: 18px; 
                         cursor: pointer;">Verify My Email</button></a>
                 <p 
-                    style="margin-top:12vh; 
+                    style="margin-top:80px; 
                     text-align: center;
                     font-size: 12px; 
-                    color: rgb(110, 110, 110);">@2023 i2Auth</div>
-            </p>
-        </div>
+                    color: rgb(110, 110, 110);">@2023 i2Auth</p>
+                </div>
     </body>
 </html>
         '''
