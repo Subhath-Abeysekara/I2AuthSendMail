994b016b93fbef6b25fdee307d2d4ef619916bea
subhath abeysekara <subhath@liveroom.dev>
2024-02-01 12:56:59 +0530

    env change

--- env.py
+++ env.py
@@ -1,5 +1,5 @@
 base_urls = {
-    "prod":"https://abv1svz0ni.execute-api.ap-south-1.amazonaws.com/prod",
-    "beta":"https://vluv1w0s7l.execute-api.ap-south-1.amazonaws.com/beta",
+    "prod":"https://fv72nzls11.execute-api.ap-south-1.amazonaws.com/prod",
+    "beta":"https://c6o7mvb9tg.execute-api.ap-south-1.amazonaws.com/beta",
     "dev":"https://zr2wffw0tl.execute-api.ap-south-1.amazonaws.com/dev"
 }
