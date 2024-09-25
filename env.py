c7445d08c06c860c2da279e651410c7ee725a589
jerom <jeromroshane911@gmail.com>
2024-09-25 15:01:34 +0530

    change env

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 base_urls = {
-    "prod":"https://fv72nzls11.execute-api.ap-south-1.amazonaws.com/prod",
+    "prod":"https://auihgk7unl.execute-api.ap-south-1.amazonaws.com/prod",
     "beta":"https://c6o7mvb9tg.execute-api.ap-south-1.amazonaws.com/beta",
     "dev":"https://zr2wffw0tl.execute-api.ap-south-1.amazonaws.com/dev",
-    "base_url":"http://52.66.100.249:5000"
+    "base_url":"http://35.154.31.110:5000/"
 }
